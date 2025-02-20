7b2e4218c9c86859956228d7ae0892a4c18ab6c6
YASHPREET-Jarora <157363952+YASHPREET-Jarora@users.noreply.github.com>
2025-02-20 13:03:41 +0000

    donedd

---
+++ Basics/03_Conversionoperations.js
@@ -0,0 +1,26 @@
+//let score = undefined;
+//let score = null;
+//let score = "33abc";
+//let score = "33";
+
+console.log(typeof (score));
+console.log(typeof score);
+let valueNum = Number(score);
+console.log(typeof (valueNum));
+console.log(valueNum);
+let str = String(valueNum);
+console.log(typeof (str));
+console.log(str);
+
+//Outputs for the types of variables -
+// "33"=> 33
+//"33abc" => NaN
+// true/false => 0/1 
+
+/*let isLoggedIn = 1;
+console.log(typeof(isLoggedIn));
+console.log(isLoggedIn);
+let boolValue = Boolean(isLoggedIn);
+console.log(typeof(boolValue));
+console.log(boolValue);*/
+
